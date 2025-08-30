8e27c896c19fdcc0085f5446bec33d9de97e761d
AmCh-Q <AmCh-Q@users.noreply.github.com>
2025-08-30 04:52:35 -0400

    Delay patchign to StaticConstructorOnStartup

--- Source/Main/Main.cs
+++ Source/Main/Main.cs
@@ -19,9 +19,21 @@ public partial class SaveFileCompression : Mod
 	public static Settings settings = null!;
 
 	public SaveFileCompression(ModContentPack content) : base(content)
+		=> settings = GetSettings<Settings>();
+
+	public override void DoSettingsWindowContents(Rect inRect)
+		=> settings!.DoSettingsWindowContents(inRect);
+
+	public override string SettingsCategory()
+		=> "SFC.Name".Translate();
+}
+
+[StaticConstructorOnStartup]
+public static class PatchApplier
+{
+	public static readonly Harmony harmony = new(id: "AmCh.SaveFileCompression");
+	static PatchApplier()
 	{
-		settings = GetSettings<Settings>();
-		Harmony harmony = new(id: "AmCh.SaveFileCompression");
 		Patches.ScribeLoader_InitLoading.Patch(harmony);
 		Patches.ScribeSaver_InitSaving.Patch(harmony);
 		Patches.ScribeSaver_FinalizeSaving.Patch(harmony);
@@ -29,10 +41,4 @@ public partial class SaveFileCompression : Mod
 		Patches.Better_ModMismatch_Window.BeginReading.Patch(harmony);
 		Patches.Better_ModMismatch_Window.EndReading.Patch(harmony);
 	}
-
-	public override void DoSettingsWindowContents(Rect inRect)
-		=> settings!.DoSettingsWindowContents(inRect);
-
-	public override string SettingsCategory()
-		=> "SFC.Name".Translate();
 }
