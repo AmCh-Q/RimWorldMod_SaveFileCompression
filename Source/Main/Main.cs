d13bb06e98b290585005203b4042918eb02353f8
AmCh-Q <AmCh-Q@users.noreply.github.com>
2025-09-05 06:38:15 -0400

    Bump version to 0.1.3

--- Source/Main/Main.cs
+++ Source/Main/Main.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using UnityEngine;
 using Verse;
 
-[assembly: AssemblyVersionAttribute("0.1.2.0")]
+[assembly: AssemblyVersionAttribute("0.1.3.0")]
 
 namespace SaveFileCompression;
 
